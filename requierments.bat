e6e980586a6308a0de5033f0c6d7b63c7f18d1ab
ghost <151090237+ghost-adx@users.noreply.github.com>
2024-07-29 21:31:07 +0200

    Create requierments.bat

---
+++ requierments.bat
@@ -0,0 +1,16 @@
+@echo off
+cls
+pip install asyncio
+pip install aiohttp
+pip install threading
+pip install colorama
+pip install sys
+pip install time
+pip install pystyle
+pip install socket
+pip install random
+pip install json
+pip install requests
+pip install os
+pip install phonenumbers
+cls
